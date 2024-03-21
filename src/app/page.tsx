a6447420380c79b819a157b85e10434036d251af
Sumanta <sumantablog@gmail.com>
2024-03-21 13:41:15 +0530

    Add new files and configurations

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-2xl">Sumanta Kabiraj </h1>
+      <div>Software Developer</div>
+    </main>
+  );
+}
